b6e423cd63ff9788f84792306f3b2d94436765d7
Taha Ahmed <taha.m8875@gmail.com>
2022-10-13 23:39:07 +0200

    B 492

---
+++ B 492 - Vanya and Lanterns/README.md
@@ -0,0 +1,3 @@
+# Vanya and Lanterns
+
+[Link on codeforces](https://codeforces.com/problemset/problem/492/B)
---
+++ B 492 - Vanya and Lanterns/java/src/com/company/Main.java
@@ -0,0 +1,71 @@
+// Codeforces 492 B - Vanya and Lanterns
+
+// Link : https://codeforces.com/problemset/problem/492/B
+
+
+//package com.company;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int numberOfLanterns = input.nextInt();
+        int streetLength = input.nextInt();
+        boolean edgeCase = false;
+
+        int[] locations = new int[numberOfLanterns];
+
+        for (int i = 0; i < numberOfLanterns; i++) {
+            locations[i] = input.nextInt();
+        }
+
+        Arrays.sort(locations);
+
+        int maxSpace = 0;
+        for (int i = 1; i < numberOfLanterns; i++) {
+            if (locations[i] - locations[i-1] > maxSpace){
+                maxSpace = locations[i] - locations[i-1];
+            }
+        }
+
+
+
+        if (locations[0] != 0){
+            if (locations[0]   > maxSpace/2.0){
+                maxSpace = locations[0];
+                edgeCase = true;
+            }
+        }
+
+        if (locations[numberOfLanterns-1] != streetLength){
+            if (!edgeCase){
+                if (streetLength - locations[numberOfLanterns-1] > maxSpace/2.0){
+                    maxSpace = streetLength - locations[numberOfLanterns-1];
+                    edgeCase = true;
+                }
+            } else if (streetLength - locations[numberOfLanterns-1] > maxSpace ){
+                maxSpace = streetLength - locations[numberOfLanterns-1];
+            }
+
+        }
+
+        if (edgeCase){
+            System.out.println(maxSpace);
+        } else {
+            System.out.println(maxSpace/2.0);
+        }
+
+    }
+
+
+
+
+}
+
