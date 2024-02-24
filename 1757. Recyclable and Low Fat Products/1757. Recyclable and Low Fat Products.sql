e1071cb7c968af01da4fbb0cdbb391b3292351bf
taham8875 <taha.m8875@gmail.com>
2024-02-24 18:48:22 +0200

    1757. Recyclable and Low Fat Products

---
+++ 1757. Recyclable and Low Fat Products/1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,4 @@
+-- Write your PostgreSQL query statement below
+-- https://leetcode.com/problems/recyclable-and-low-fat-products/
+select product_id from products
+where low_fats = 'Y' and recyclable = 'Y'
