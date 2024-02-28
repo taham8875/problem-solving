d8edbddd8bea835351bfd88ec1f27276c355a94a
taham8875 <taha.m8875@gmail.com>
2024-02-28 21:36:34 +0200

    1789. Primary Department for Each Employee

---
+++ 1789. Primary Department for Each Employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,15 @@
+-- Write your PostgreSQL query statement below
+SELECT employee_id,
+    department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+UNION
+SELECT employee_id,
+    department_id
+FROM Employee
+WHERE employee_id IN (
+        SELECT employee_id
+        FROM Employee
+        GROUP BY employee_id
+        HAVING COUNT(employee_id) = 1
+    )
