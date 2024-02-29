48d624556862ed9b86bc7faedf3ce8163a8d1e29
taham8875 <taha.m8875@gmail.com>
2024-02-29 09:30:14 +0200

    1141. User Activity for the Past 30 Days I

---
+++ 1141. User Activity for the Past 30 Days I/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+SELECT activity_date AS date,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE '2019-07-27' - activity_date <= 30
+GROUP BY activity_date
